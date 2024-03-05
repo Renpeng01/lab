21b28bf79e6c4e28bf6222fe6ffa0a7bf08876f8
renpeng <renpeng@hellogroup.com>
2024-03-05 17:59:26 +0800

    add

---
+++ go/src/test/iota.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	mutexLocked = 1 << iota // mutex is locked
+	mutexWoken
+	mutexStarving
+	mutexWaiterShift = iota
+
+	starvationThresholdNs = 1e6
+)
+
+func main() {
+	fmt.Println("mutexLocked: ", mutexLocked, " mutexWoken: ", mutexWoken)
+}
