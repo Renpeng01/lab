43b7c49abd9a0f743478c8e3fdbcd5ed26fa8009
renpeng <renpeng@hellogroup.com>
2024-03-07 18:29:01 +0800

    add

--- go/src/go_link_name_interface/go_linkname_interface.go
+++ go/src/go_link_name_interface/go_linkname_interface.go
@@ -1,14 +1,7 @@
 package go_link_name_interface
 
 import (
-	_ "unsafe"
-
 	_ "lab/src/go_link_name_src"
 )
 
-//go:linkname golinknameinterface lab/src/go_link_name_src.golinknamesrc
-func golinknameinterface()
-
-func GolinknameinterfaceTest() {
-	golinknameinterface()
-}
+func GolinknameinterfaceTest()
--- go/src/go_link_name_src/go_linkname_src.go
+++ go/src/go_link_name_src/go_linkname_src.go
@@ -1,7 +1,11 @@
 package go_link_name_src
 
-import "fmt"
+import (
+	"fmt"
+	_ "unsafe"
+)
 
+//go:linkname golinknamesrc lab/src/go_link_name_interface.GolinknameinterfaceTest
 func golinknamesrc() {
 	fmt.Println(111111)
 }
